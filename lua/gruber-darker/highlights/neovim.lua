18aa5bc684ae2f518dcc2afb3136554f48649c1d
Nikita Kulakov <klephron.dev@gmail.com>
2026-01-12 08:12:47 +0300

    rm bold for contants and bools

--- lua/gruber-darker/highlights/neovim.lua
+++ lua/gruber-darker/highlights/neovim.lua
@@ -117,11 +117,11 @@ M["WildMenu"]         = Highlight.new("WildMenu", { fg = c["black"], bg = c["yel
 
 
 M["Comment"]        = Highlight.new("Comment", { fg = c["brown"], italic = cfg.italic.comment })
-M["Constant"]       = Highlight.new("Constant", { fg = c["quartz"], bold = cfg.bold })
+M["Constant"]       = Highlight.new("Constant", { fg = c["quartz"] })
 M["String"]         = Highlight.new("String", { fg = c["green"], italic = cfg.italic.string })
 M["Character"]      = Highlight.new("Character", { fg = c["green"], italic = cfg.italic.character })
 M["Number"]         = Highlight.new("Number", { fg = c["wisteria"] })
-M["Boolean"]        = Highlight.new("Boolean", { fg = c["wisteria"], bold = cfg.bold })
+M["Boolean"]        = Highlight.new("Boolean", { fg = c["wisteria"] })
 M["Float"]          = Highlight.new("Float", { fg = c["wisteria"] })
 M["Identifier"]     = Highlight.new("Identifier", { fg = c["fg"] })
 M["Function"]       = Highlight.new("Function", { fg = c["niagara"] })
